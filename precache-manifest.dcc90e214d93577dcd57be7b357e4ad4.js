8098750d174cc0175219948f3ca8f9b79a869364
Sebastian Sutkowski <sebastian.sutkowski1@gmail.com>
2020-06-01 09:08:57 +0200

    Updates

---
+++ precache-manifest.dcc90e214d93577dcd57be7b357e4ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80f61685ec2e514ea756d2b4fe282fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de94fc9ae802a9e80e94",
+    "url": "/static/css/main.d7c5c9b5.chunk.css"
+  },
+  {
+    "revision": "fa2a0b0b871c3b55d1d9",
+    "url": "/static/js/2.7a3e2660.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7a3e2660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de94fc9ae802a9e80e94",
+    "url": "/static/js/main.99a0566d.chunk.js"
+  },
+  {
+    "revision": "cafde2c6a6bfbbc0782c",
+    "url": "/static/js/runtime-main.d4a0110e.js"
+  }
+]);
