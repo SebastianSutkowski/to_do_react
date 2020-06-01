21467f813b76d6090efed342ded45e27151f5716
Sebastian Sutkowski <sebastian.sutkowski1@gmail.com>
2020-06-01 09:31:51 +0200

    Updates

---
+++ precache-manifest.4ae2e87e26b90a2779dc83a97e93aafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0edc8f9c4209ea8f9ebf17879f5c42",
+    "url": "/to_do_react/index.html"
+  },
+  {
+    "revision": "509c81b99ca3fa85389a",
+    "url": "/to_do_react/static/css/main.d7c5c9b5.chunk.css"
+  },
+  {
+    "revision": "fa2a0b0b871c3b55d1d9",
+    "url": "/to_do_react/static/js/2.7a3e2660.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/to_do_react/static/js/2.7a3e2660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509c81b99ca3fa85389a",
+    "url": "/to_do_react/static/js/main.33bc0865.chunk.js"
+  },
+  {
+    "revision": "817114802a8f8b2ab8f4",
+    "url": "/to_do_react/static/js/runtime-main.d114af29.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcc90e214d93577dcd57be7b357e4ad4.js"
+  "/to_do_react/precache-manifest.4ae2e87e26b90a2779dc83a97e93aafe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to_do_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
